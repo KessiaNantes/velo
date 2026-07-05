ef3006b25545669a26ce0724a869fc2f1b1e3500
KessiaNantes <kessia.nandes@gmail.com>
2026-07-05 20:52:20 -0300

    AAA - Arrange, act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,19 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/');
-
-  // Checkpoint 1: Verificar se a página de consulta de pedidos está visível
   await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //Checkpoint 2: Verificar se a página de consulta de pedidos está visível
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-QQEIZU');
-
   await page.getByTestId('search-order-button').click();
   
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-QQEIZU');
 
